1ee42f3003b522a9505769109a108433e09ea076
Tommaso Tiso <tommaso.tiso@gmail.com>
2023-03-10 19:05:20 +0100

    nuovi esercizi

---
+++ 08-CardsDeckMechanic/index.js
@@ -0,0 +1,26 @@
+class Deck {
+  constructor() {
+    this.carte = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "fante", "donna", "re"];
+  }
+  mischia() {
+    this.carte.sort((a, b) => 0.5 - Math.random());
+    console.log("mazzo mischiato", this.carte);
+  }
+
+  pesca() {
+    // const carta = this.carte.shift(); -> logica inversa
+    const carta = this.carte.pop();
+    console.log("carta pescata", carta);
+    console.log("mazzo dopo la pescate", this.carte);
+  }
+  mettiSotto(carta) {
+    // this.carte.push(carta); -> logica inversa
+    this.carte.unshift(carta);
+    console.log("mazzo dopo metti sotto", this.carte);
+  }
+}
+
+const deck = new Deck();
+deck.mischia();
+deck.pesca();
+deck.mettiSotto("jolly");
---
+++ 09-mappingJSON/index.js
@@ -0,0 +1,19 @@
+const data = {
+  users: {
+    wqiu237uh390fvx: {
+      firstname: "luca",
+      lastname: "rossi",
+    },
+    ksxz993ql617oer: {
+      firstname: "marco",
+      lastname: "verdi",
+    },
+  },
+};
+
+const users = [];
+
+for (let key of Object.keys(data.users)) {
+  users.push({ id: key, ...data.users });
+}
+console.log(users);
